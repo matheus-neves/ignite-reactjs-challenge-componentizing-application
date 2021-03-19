beedc36adbda691ad816f5113644009f1585104e
Matheus Neves <matheusneves1989@gmail.com>
2021-03-18 21:09:01 -0300

    refactor: Componentizing sidebar and content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,44 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button"
+import { GenreResponseProps } from '../App'
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+
+interface SideBarProps {
+  handleSelectedGenreId: React.Dispatch<React.SetStateAction<number>>;
+  selectedGenreId: number;
+}
+
+export const SideBar = ({ handleSelectedGenreId, selectedGenreId }: SideBarProps) => {
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  const handleClickButton = (id: number) => {
+    handleSelectedGenreId(id)
+  }
+
+  return (
+
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
